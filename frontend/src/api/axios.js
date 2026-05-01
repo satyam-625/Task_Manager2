504366ce529a068871256a27b021d29fa16cd306
Satyam Kashyap <kashyapsatyam555@gmail.com>
2026-05-01 23:12:02 +0530

    update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -6,7 +6,9 @@ import axios from 'axios';
 // });
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || 'taskmanager2-production-91e1.up.railway.app',
+
+ // baseURL: "http://localhost:5000/api",
   headers: { 'Content-Type': 'application/json' }
 });
 
